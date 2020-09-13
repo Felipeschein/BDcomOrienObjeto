091227eb54d1c934832d8078a17395e9db4c18bd
Felipe Schein <felipe_str@hotmail.com>
2020-09-13 16:37:20 -0300

    Acesso ao Banco de Dados SQL e gravação em uma lista com orientação a objeto.

---
+++ BDOrientacaoObjeto/Banco.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace BDOrientacaoObjeto
+{
+    public class Banco
+    {
+        private SqlConnection _banco { get; set; }  //toda propriedade privada dentro de uma classe deve iniciar com _ 
+
+        public Banco(string nomeServidor, string nomeBancodeDados)
+        {
+            _banco = new SqlConnection("Server=" + nomeServidor + ";Database=" + nomeBancodeDados + "; Integrated Security=True;");
+            _banco.Open();
+        }
+        //método público inicia com maíuscula e com verbo de ação
+        public void ExecutarComando(string comandoTexto)
+        {
+            var comando = new SqlCommand(comandoTexto, _banco);
+            comando.ExecuteNonQuery();
+        }
+
+    }
+}
---
+++ BDOrientacaoObjeto/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDOrientacaoObjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var banco = new Banco("DESKTOP-IMST3LM\\SQLEXPRESS", "DBLICKOSKITEACHER");
+
+            var listaDeNomes = new List<string>();
+            listaDeNomes.Add("Tiago Lucca");
+            listaDeNomes.Add("Jeferson");
+            listaDeNomes.Add("Bruno");
+            listaDeNomes.Add("Silas");
+            listaDeNomes.Add("Leticia");
+
+            foreach (var item in listaDeNomes)
+            {
+                banco.ExecutarComando("insert into PessoasNome(nome) values('" + item + "')");
+            }
+
+            
+        }
+    }
+}
